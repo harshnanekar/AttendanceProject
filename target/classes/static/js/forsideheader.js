460ff95bec982d23a01759d9822c090385ba4669
harshnanekar <nanekarharshal2@gmail.com>
2023-10-28 11:16:12 +0530

    adding files

---
+++ target/classes/static/js/forsideheader.js
@@ -0,0 +1,18 @@
+  var elements= document.querySelector(".aside");
+  
+  if(elements){
+ elements.addEventListener('click',function(){
+
+    console.log("button clicked");
+    var style = getComputedStyle(document.body);
+    let asideVAlue = style.getPropertyValue("--sidenav");
+    console.log(asideVAlue);
+    if(asideVAlue == '350px'){
+
+        document.documentElement.style.setProperty("--sidenav", "0px");
+
+    }else{
+        document.documentElement.style.setProperty("--sidenav", "350px");
+    }
+});
+  }
